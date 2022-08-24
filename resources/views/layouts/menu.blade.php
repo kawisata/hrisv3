be5434d1dab3e5a51e8a5d7db8ce0c2d04aee5b3
indorailtour2012 <108383221+indorailtour2012@users.noreply.github.com>
2022-08-24 14:08:51 +0700

    update

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -262,7 +262,7 @@
                   <li
                     class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                       >
-                    <a class="w-full" href="{{ route('MemberReduksiFrontlinerUpdate.index') }}">Update Member Reduksi Frontliner</a>
+                    <a class="w-full" href="{{ route('MemberReduksiFrontlinerUpdate') }}">Update Member Reduksi Frontliner</a>
                   </li>
                   <li
                     class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
